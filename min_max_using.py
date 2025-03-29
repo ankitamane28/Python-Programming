dae8a9d71c7cb073d03098d9ae53b16f1aedaefe
ankitamane28 <maneanku451@gmail.com>
2025-03-29 22:08:10 +0530

    min_max_using.py

---
+++ min_max_using.py
@@ -0,0 +1,8 @@
+import numpy as np
+salse = np.array([5000, 7000, 8000, 6500, 7200,9000, 8500])
+
+highest_salse = np.max(salse)
+lowest_salse = np.min(salse)
+
+print("highest salse",highest_salse)
+print("lowest salse",lowest_salse)
